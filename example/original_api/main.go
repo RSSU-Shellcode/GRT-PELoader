2b51a64c781ba1951f679535f8c747d5be3c4e49
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-25 13:51:42 +0800

    original_api/main.go: add example for show how to call original API.

---
+++ example/original_api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/sys/windows"
+
+	"github.com/RSSU-Shellcode/Gleam-RT/runtime"
+)
+
+var (
+	modKernel32 = windows.NewLazySystemDLL("kernel32.dll")
+
+	procVirtualAlloc = modKernel32.NewProc("VirtualAlloc")
+)
+
+func main() {
+	redirected := procVirtualAlloc.Addr()
+	original, err := gleamrt.GetProcAddressOriginal(modKernel32.Handle(), "VirtualAlloc")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("redirected: 0x%X\n", redirected)
+	fmt.Printf("original:   0x%X\n", original)
+
+	if redirected == original {
+		panic("redirected equal original procedure address")
+	}
+}
